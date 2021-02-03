e61b4247d5cae9cad2be644a682ae33c68a2b951
Valdeir Júnior <vjrjunior@gmail.com>
2021-02-03 10:55:20 -0300

    general updates

--- services/web-server.js
+++ services/web-server.js
@@ -4,12 +4,12 @@ const morgan = require('morgan');
 const webServerConfig = require('../config/web-server.js');
 const router = require('./router.js');
 
-let httpServer;
+const server;
 
 function initialize() {
   return new Promise((resolve, reject) => {
     const app = express();
-    httpServer = http.createServer(app);
+    server = http.createServer(app);
 
     // Combines logging info from request and response
     app.use(morgan('combined'));
@@ -22,7 +22,7 @@ function initialize() {
     // Mount the router at /api so all its routes start with /api
     app.use('/api', router);
 
-    httpServer.listen(webServerConfig.port)
+    server.listen(webServerConfig.port)
       .on('listening', () => {
         console.log(`Web server listening on localhost:${webServerConfig.port}`);
 
@@ -38,7 +38,7 @@ module.exports.initialize = initialize;
 
 function close() {
   return new Promise((resolve, reject) => {
-    httpServer.close((err) => {
+    server.close((err) => {
       if (err) {
         reject(err);
         return;
